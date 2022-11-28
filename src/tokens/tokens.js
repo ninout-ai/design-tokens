9eeccc9d7737e993f8bcd420300ca9704de1c250
takehiro <takehiro-r133@hotmail.co.jp>
2022-11-28 18:12:58 +0900

    token-transformer, figma-tokens導入 #5

---
+++ src/tokens/tokens.js
@@ -0,0 +1,435 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 26 Nov 2022 12:10:05 GMT
+ */
+
+module.exports = {
+  "tokenSetOrder": {
+    "0": {
+      "value": "primitive",
+      "type": "other",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "primitive",
+        "type": "other"
+      },
+      "name": "TokenSetOrder0",
+      "attributes": {
+        "category": "tokenSetOrder",
+        "type": "0"
+      },
+      "path": [
+        "tokenSetOrder",
+        "0"
+      ]
+    },
+    "1": {
+      "value": "semantic",
+      "type": "other",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "semantic",
+        "type": "other"
+      },
+      "name": "TokenSetOrder1",
+      "attributes": {
+        "category": "tokenSetOrder",
+        "type": "1"
+      },
+      "path": [
+        "tokenSetOrder",
+        "1"
+      ]
+    }
+  },
+  "color": {
+    "text": {
+      "light": {
+        "body": {
+          "value": "#333333",
+          "type": "color",
+          "filePath": "output.json",
+          "isSource": true,
+          "original": {
+            "value": "#333333",
+            "type": "color"
+          },
+          "name": "ColorTextLightBody",
+          "attributes": {
+            "category": "color",
+            "type": "text",
+            "item": "light",
+            "subitem": "body"
+          },
+          "path": [
+            "color",
+            "text",
+            "light",
+            "body"
+          ]
+        }
+      }
+    },
+    "white": {
+      "value": "#ffffff",
+      "type": "color",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "#FFFFFF",
+        "type": "color"
+      },
+      "name": "ColorWhite",
+      "attributes": {
+        "category": "color",
+        "type": "white"
+      },
+      "path": [
+        "color",
+        "white"
+      ]
+    },
+    "black": {
+      "value": "#000000",
+      "type": "color",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "#000000",
+        "type": "color"
+      },
+      "name": "ColorBlack",
+      "attributes": {
+        "category": "color",
+        "type": "black"
+      },
+      "path": [
+        "color",
+        "black"
+      ]
+    },
+    "gray": {
+      "50": {
+        "value": "#f6f6f6",
+        "type": "color",
+        "filePath": "output.json",
+        "isSource": true,
+        "original": {
+          "value": "#F6F6F6",
+          "type": "color"
+        },
+        "name": "ColorGray50",
+        "attributes": {
+          "category": "color",
+          "type": "gray",
+          "item": "50"
+        },
+        "path": [
+          "color",
+          "gray",
+          "50"
+        ]
+      },
+      "100": {
+        "value": "#eaeaea",
+        "type": "color",
+        "filePath": "output.json",
+        "isSource": true,
+        "original": {
+          "value": "#EAEAEA",
+          "type": "color"
+        },
+        "name": "ColorGray100",
+        "attributes": {
+          "category": "color",
+          "type": "gray",
+          "item": "100"
+        },
+        "path": [
+          "color",
+          "gray",
+          "100"
+        ]
+      },
+      "200": {
+        "value": "#e0e0e0",
+        "type": "color",
+        "filePath": "output.json",
+        "isSource": true,
+        "original": {
+          "value": "#E0E0E0",
+          "type": "color"
+        },
+        "name": "ColorGray200",
+        "attributes": {
+          "category": "color",
+          "type": "gray",
+          "item": "200"
+        },
+        "path": [
+          "color",
+          "gray",
+          "200"
+        ]
+      },
+      "300": {
+        "value": "#bababa",
+        "type": "color",
+        "filePath": "output.json",
+        "isSource": true,
+        "original": {
+          "value": "#BABABA",
+          "type": "color"
+        },
+        "name": "ColorGray300",
+        "attributes": {
+          "category": "color",
+          "type": "gray",
+          "item": "300"
+        },
+        "path": [
+          "color",
+          "gray",
+          "300"
+        ]
+      },
+      "400": {
+        "value": "#999999",
+        "type": "color",
+        "filePath": "output.json",
+        "isSource": true,
+        "original": {
+          "value": "#999999",
+          "type": "color"
+        },
+        "name": "ColorGray400",
+        "attributes": {
+          "category": "color",
+          "type": "gray",
+          "item": "400"
+        },
+        "path": [
+          "color",
+          "gray",
+          "400"
+        ]
+      },
+      "500": {
+        "value": "#7c7c7c",
+        "type": "color",
+        "filePath": "output.json",
+        "isSource": true,
+        "original": {
+          "value": "#7C7C7C",
+          "type": "color"
+        },
+        "name": "ColorGray500",
+        "attributes": {
+          "category": "color",
+          "type": "gray",
+          "item": "500"
+        },
+        "path": [
+          "color",
+          "gray",
+          "500"
+        ]
+      },
+      "600": {
+        "value": "#6f6f6f",
+        "type": "color",
+        "filePath": "output.json",
+        "isSource": true,
+        "original": {
+          "value": "#6F6F6F",
+          "type": "color"
+        },
+        "name": "ColorGray600",
+        "attributes": {
+          "category": "color",
+          "type": "gray",
+          "item": "600"
+        },
+        "path": [
+          "color",
+          "gray",
+          "600"
+        ]
+      },
+      "700": {
+        "value": "#4b4b4b",
+        "type": "color",
+        "filePath": "output.json",
+        "isSource": true,
+        "original": {
+          "value": "#4B4B4B",
+          "type": "color"
+        },
+        "name": "ColorGray700",
+        "attributes": {
+          "category": "color",
+          "type": "gray",
+          "item": "700"
+        },
+        "path": [
+          "color",
+          "gray",
+          "700"
+        ]
+      },
+      "750": {
+        "value": "#3e3e3e",
+        "type": "color",
+        "filePath": "output.json",
+        "isSource": true,
+        "original": {
+          "value": "#3E3E3E",
+          "type": "color"
+        },
+        "name": "ColorGray750",
+        "attributes": {
+          "category": "color",
+          "type": "gray",
+          "item": "750"
+        },
+        "path": [
+          "color",
+          "gray",
+          "750"
+        ]
+      },
+      "800": {
+        "value": "#494949",
+        "type": "color",
+        "filePath": "output.json",
+        "isSource": true,
+        "original": {
+          "value": "#494949",
+          "type": "color"
+        },
+        "name": "ColorGray800",
+        "attributes": {
+          "category": "color",
+          "type": "gray",
+          "item": "800"
+        },
+        "path": [
+          "color",
+          "gray",
+          "800"
+        ]
+      },
+      "900": {
+        "value": "#333333",
+        "type": "color",
+        "filePath": "output.json",
+        "isSource": true,
+        "original": {
+          "value": "#333333",
+          "type": "color"
+        },
+        "name": "ColorGray900",
+        "attributes": {
+          "category": "color",
+          "type": "gray",
+          "item": "900"
+        },
+        "path": [
+          "color",
+          "gray",
+          "900"
+        ]
+      }
+    },
+    "brand": {
+      "cs-lightgray": {
+        "value": "#ada6a1",
+        "type": "color",
+        "filePath": "output.json",
+        "isSource": true,
+        "original": {
+          "value": "#ADA6A1",
+          "type": "color"
+        },
+        "name": "ColorBrandCsLightgray",
+        "attributes": {
+          "category": "color",
+          "type": "brand",
+          "item": "cs-lightgray"
+        },
+        "path": [
+          "color",
+          "brand",
+          "cs-lightgray"
+        ]
+      },
+      "cs-orange": {
+        "value": "#f49e00",
+        "type": "color",
+        "filePath": "output.json",
+        "isSource": true,
+        "original": {
+          "value": "#F49E00",
+          "type": "color"
+        },
+        "name": "ColorBrandCsOrange",
+        "attributes": {
+          "category": "color",
+          "type": "brand",
+          "item": "cs-orange"
+        },
+        "path": [
+          "color",
+          "brand",
+          "cs-orange"
+        ]
+      },
+      "cs-darkgray": {
+        "value": "#6a6a6a",
+        "type": "color",
+        "filePath": "output.json",
+        "isSource": true,
+        "original": {
+          "value": "#6A6A6A",
+          "type": "color"
+        },
+        "name": "ColorBrandCsDarkgray",
+        "attributes": {
+          "category": "color",
+          "type": "brand",
+          "item": "cs-darkgray"
+        },
+        "path": [
+          "color",
+          "brand",
+          "cs-darkgray"
+        ]
+      }
+    },
+    "blue": {
+      "500-primary": {
+        "value": "#73b1c0",
+        "type": "color",
+        "filePath": "output.json",
+        "isSource": true,
+        "original": {
+          "value": "#73B1C0",
+          "type": "color"
+        },
+        "name": "ColorBlue500Primary",
+        "attributes": {
+          "category": "color",
+          "type": "blue",
+          "item": "500-primary"
+        },
+        "path": [
+          "color",
+          "blue",
+          "500-primary"
+        ]
+      }
+    }
+  }
+};
